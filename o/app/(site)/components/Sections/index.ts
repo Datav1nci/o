1896c94bd64fa42eb3501eef749547a9c9a18762
Datav1nci <jestais4@gmail.com>
2025-09-05 23:59:51 -0400

    added problem section

--- o/app/(site)/components/Sections/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as WholeHouse } from './WholeHouse';
-export { default as DrinkingWater } from './DrinkingWater';
-export { default as Problems } from './Problems';
-export { default as Techniques } from './Techniques';
-export { default as Distribution } from './Distribution';
--- o/app/(site)/page.tsx
+++ o/app/(site)/page.tsx
@@ -3,12 +3,14 @@ import WholeHouse from './components/Sections/WholeHouse';
 import DrinkingWater from './components/Sections/DrinkingWater';
 import Techniques from './components/Sections/Techniques';
 import Distribution from './components/Sections/Distribution';
+import Problems from './components/Sections/Problems'; 
 
 
 export default function Page() {
 return (
 <>
 <Hero />
+<Problems /> 
 <WholeHouse />
 <DrinkingWater />
 <Techniques />
