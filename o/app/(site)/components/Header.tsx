891e4dbe064b1303ee880b09982414fe8d700a7b
Datav1nci <jestais4@gmail.com>
2025-09-06 01:34:51 -0400

    logo 2

--- o/app/(site)/components/Header.tsx
+++ o/app/(site)/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link href="/" className="flex items-center gap-2 shrink-0">
           <Image
-            src="/public/images/logo.png"   
+            src="/images/logo2.png"   
             alt="Ö Home"
             width={40}
             height={40}
