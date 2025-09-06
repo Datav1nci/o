6f508f6fbd3e2f68cb6206e9549ba087d73e0d0a
Datav1nci <jestais4@gmail.com>
2025-09-06 14:08:40 -0400

    syntaxt consistancy

--- o/app/(site)/page.tsx
+++ o/app/(site)/page.tsx
@@ -1,19 +1,20 @@
+// app/(site)/page.tsx
 import Hero from './components/Hero';
 import WholeHouse from './components/Sections/WholeHouse';
 import DrinkingWater from './components/Sections/DrinkingWater';
 import Techniques from './components/Sections/Techniques';
 import Distribution from './components/Sections/Distribution';
-import Problem from './components/Sections/Problems';
+import Problems from './components/Sections/Problems';
 
 export default function Page() {
-return (
-<>
-<Hero />
-<Problem />
-<WholeHouse />
-<DrinkingWater />
-<Techniques />
-<Distribution />
-</>
-);
+  return (
+    <>
+      <Hero />
+      <Problems />
+      <WholeHouse />
+      <DrinkingWater />
+      <Techniques />
+      <Distribution />
+    </>
+  );
 }
